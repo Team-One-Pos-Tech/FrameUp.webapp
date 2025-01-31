291663320917c1e2fc850ae90676de4be422579c
Kássio San <kciosantana@gmail.com>
2025-01-31 16:47:28 -0300

    bind model and form

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  loginForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  onSubmit() {
+    const username = this.loginForm.value.username;
+    const password = this.loginForm.value.password;
+
+    console.log('Form submitted');
+    console.log('username: ', username);
+    console.log('password: ', password);
+  }
 }
