752f607c6f2984accb02435e6d8fce6d43befde0
Kássio San <kciosantana@gmail.com>
2025-01-31 17:20:24 -0300

    generate openapi files

---
+++ src/app/infra/snackhub-api/apis/index.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AuthenticationApi';
+export * from './ClientApi';
+export * from './KitchenOrderApi';
+export * from './OrderApi';
+export * from './ProductApi';
+export * from './WebhookApi';
