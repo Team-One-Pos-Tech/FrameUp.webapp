e7cc7ba01cd20e8e41884272727f23dbf4aec01c
Kássio San <kciosantana@gmail.com>
2025-01-31 23:54:28 -0300

    init project

--- src/app/infra/snackhub-api/apis/index.ts
+++
@@ -1,8 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './AuthenticationApi';
-export * from './ClientApi';
-export * from './KitchenOrderApi';
-export * from './OrderApi';
-export * from './ProductApi';
-export * from './WebhookApi';
