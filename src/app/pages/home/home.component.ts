fe0cef6ccbc8088ac7fa516a17ce646c67332dce
Kássio San <kciosantana@gmail.com>
2025-02-08 02:47:43 -0300

    get user logged

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import AuthenticationService, { CurrentUser } from '../../services/authentication.service';
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  currentUser: CurrentUser | null = null;
 
+  constructor(private authService: AuthenticationService) {}
+
+  ngOnInit() {
+    this.currentUser = this.authService.getCurrentUser();
+  }
 }
