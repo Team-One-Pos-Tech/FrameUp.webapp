e02d49557106e028b4f4adaed5758b3f9a85af8b
Kássio San <kciosantana@gmail.com>
2025-02-08 17:17:34 -0300

    update env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    identityApiUrl: 'http://51.8.26.113:5001',
-    orderApiUrl: 'http://48.217.14.253:5003'
+    identityApiUrl: 'http://51.8.211.123:5001',
+    orderApiUrl: 'http://135.234.229.207:5003'
 };
